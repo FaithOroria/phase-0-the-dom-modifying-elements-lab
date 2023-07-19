352f7ce7e99d02c039e10a0e6c87967cdb71a4a7
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-07-20 01:21:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainElement = document.querySelector("main");
+mainElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Faith is the champion";
+document.body.appendChild(newHeader);
+
